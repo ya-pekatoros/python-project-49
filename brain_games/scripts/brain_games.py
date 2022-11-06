fa8e69655cc09f7c4380346b4f7fb2de1c99eb7b
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2022-11-06 17:09:10 +0300

    Add first version of brain_games (only Greegings)

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+''' Initialization of the game from bash'''
+
+def main():
+    ''' Greetings'''
+    print('Welcome to the Brain Games!')
+
+if __name__ == '__main__':
+    main()
